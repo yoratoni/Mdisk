6f32b7e7e743b89943537b9aa2f98f3b40c5ef8f
yoratoni <71235233+yoratoni@users.noreply.github.com>
2022-12-05 03:42:39 +0100

    TS setup

---
+++ .eslintrc.js
@@ -0,0 +1,85 @@
+module.exports = {
+    plugins: [
+        "@typescript-eslint",
+        "import"
+    ],
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    settings: {
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts"]
+        },
+        "import/resolver": {
+            "typescript": {
+                "alwaysTryTypes": true,
+                "project": "packages/*/tsconfig.json"
+            }
+        },
+    },
+    rules: {
+        "indent": "off",
+        "@typescript-eslint/indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "double"
+        ],
+        "semi": [
+            "warn",
+            "always"
+        ],
+        "object-curly-spacing": [
+            "warn",
+            "always"
+        ],
+        "sort-imports": [
+            "warn",
+            {
+                ignoreCase: false,
+                ignoreDeclarationSort: true,
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+                allowSeparatedGroups: true
+            }
+        ],
+        "import/no-unresolved": "error",
+        "import/order": [
+            "warn",
+            {
+                groups: [
+                    "builtin",
+                    "external",
+                    "internal",
+                    ["sibling", "parent"],
+                    "index",
+                    "unknown"
+                ],
+                "newlines-between": "always",
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true
+                }
+            }
+        ]
+    }
+};
